1086afda0bdd27d2627ca2b9d5f47bbafb75ef52
mwcz <mwc@clayto.com>
2022-12-03 21:57:29 -0500

    multiplying by zero equals zero

--- src/day3.rs
+++ src/day3.rs
@@ -1,4 +1,4 @@
-use aoc_runner_derive::{aoc, aoc_generator};
+use aoc_runner_derive::aoc;
 use num_bigint::BigUint;
 use once_cell::sync::Lazy;
 
@@ -6,11 +6,13 @@ use once_cell::sync::Lazy;
 // rucksack products :(
 // hm, cuviper's num-bigint would help, maybe I'll go for that.  seems fun.
 
-const CAPS_OFFSET: usize = 65 - 26;
-const LOWER_OFFSET: usize = 97;
+#[rustfmt::skip]
 const PRIMES_U32: [u32; 52] = [
+/*  a  b  c  d   e   f   g   h   i   j   k   l   m   n   o   p   q   r   s   t   u   v   w   x   y */
     2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+/*    z    A    B    C    D    E    F    G    H    I    J    K    L    M    N    O    P    Q    R */
     101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193,
+/*    S    T    U    V    W    X    Y    Z */
     197, 199, 211, 223, 227, 229, 233, 239,
 ];
 static PRIMES: Lazy<[BigUint; 52]> = Lazy::new(|| PRIMES_U32.map(BigUint::from));
@@ -18,6 +20,9 @@ static PRIMES: Lazy<[BigUint; 52]> = Lazy::new(|| PRIMES_U32.map(BigUint::from))
 /// Map the chars 'a'..'z' and then 'A'..'Z' to the first 52 prime numbers and return an Item
 /// containing the prime and the priority of the letter.
 fn prime(c: char) -> Item<'static> {
+    const CAPS_OFFSET: usize = 65 - 26;
+    const LOWER_OFFSET: usize = 97;
+
     let offset = match c {
         'a'..='z' => c as usize - LOWER_OFFSET,
         'A'..='Z' => c as usize - CAPS_OFFSET,
@@ -60,8 +65,10 @@ fn part1_solve(input: &str) -> usize {
     for rucksack in rucksacks.iter() {
         // find the product of compartment 1's prime numbers, then check each of compartment 0's
         // prime numbers to see if they evenly divide into compartment 1's product.
-        let compartment1_product: &BigUint =
-            &rucksack.1.iter().fold(BigUint::default(), |acc, item| acc * item.0);
+        let compartment1_product: &BigUint = &rucksack
+            .1
+            .iter()
+            .fold(BigUint::from(1u32), |acc, item| acc * item.0);
         for (prime, priority) in &rucksack.0 {
             // if the prime evently divides into compartment 1's product, then the letter exists in
             // that compartment too, so it's what we're looking for.
@@ -95,7 +102,8 @@ mod day3_tests {
     #[test]
     fn part1_solve_test() {
         assert_eq!(
-            part1_solve("vJrwpWtwJgWrhcsFMMfFFhFp
+            part1_solve(
+                "vJrwpWtwJgWrhcsFMMfFFhFp
 jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
 PmmdzqPrVvPwwTWBwg
 wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
