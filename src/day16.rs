d66c09a14783b48cb84abde3912e3a4f7dd35ace
mwcz <mwc@clayto.com>
2022-12-22 00:48:12 -0500

    day 16...

--- src/day16.rs
+++ src/day16.rs
@@ -1,8 +1,6 @@
 use aoc_runner_derive::aoc;
-use itertools::Itertools;
 use petgraph::algo::floyd_warshall;
 use petgraph::prelude::*;
-use rayon::prelude::*;
 use std::collections::HashMap;
 
 const START: &str = "AA";
@@ -13,6 +11,7 @@ struct Valve<'name> {
     exits: Vec<String>,
 }
 
+/// The data about a valve that's added to the graph as a node "weight".
 #[derive(PartialEq, Eq, Hash, PartialOrd, Ord, Copy, Clone)]
 struct ValveData<'name> {
     name: &'name str,
@@ -27,19 +26,36 @@ impl<'name> std::fmt::Debug for ValveData<'name> {
 
 #[derive(Debug)]
 struct Cave<'name> {
-    graph: DiGraphMap<ValveData<'name>, u32>,
+    graph: GraphMap<ValveData<'name>, u32, Directed>,
     start: Option<ValveData<'name>>,
     dists: Option<HashMap<(ValveData<'name>, ValveData<'name>), u32>>,
-    i: usize,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct Player<'name> {
+    /// The valve the player is currently standing by.
+    dest: Option<ValveData<'name>>,
+    position: ValveData<'name>,
+    /// Time left until this player can move again.
+    ttm: u32,
+}
+
+impl<'name> Player<'name> {
+    fn new(position: ValveData<'name>) -> Self {
+        Self {
+            position,
+            dest: None,
+            ttm: 0,
+        }
+    }
 }
 
 impl<'input> Cave<'input> {
     fn new(valves: Vec<Valve<'input>>) -> Self {
         let mut cave = Self {
-            graph: DiGraphMap::new(),
+            graph: GraphMap::new(),
             start: None,
             dists: None,
-            i: 0,
         };
 
         // add valve nodes
@@ -78,16 +94,14 @@ impl<'input> Cave<'input> {
             );
 
             // remove all edges and replace them with edges connecting every node with every other
-            // node
-            // was fun but disabling for now because with the dists already available it doesn't
-            // really help to have them built into the graph edges.
+            // node, to eliminate the need to ever do pathfinding.
 
-            // for ((a, b), dist) in shortest_paths.iter() {
-            //     cave.graph.remove_edge(*a, *b);
-            //     if a != b {
-            //         cave.graph.add_edge(*a, *b, *dist);
-            //     }
-            // }
+            for ((a, b), dist) in shortest_paths.iter() {
+                cave.graph.remove_edge(*a, *b);
+                if a != b {
+                    cave.graph.add_edge(*a, *b, *dist);
+                }
+            }
         }
 
         cave
@@ -122,33 +136,178 @@ impl<'input> Cave<'input> {
         }
     }
 
-    fn scan(&self, start: ValveData<'input>, total_time: u32) -> u32 {
-        let paths = self
-            .graph
-            .nodes()
-            .filter(|valve| valve.name != start.name)
-            .permutations(
-                self.graph.node_count() - 1, /* since start is omitted */
-            )
-            .par_bridge();
+    fn visit(&self, visited: Vec<ValveData<'input>>, remaining_time: u32, total_time: u32) -> u32 {
+        let path_str: Vec<&str> = visited.iter().map(|valve| valve.name).collect();
+        // println!("{:?} @ {} minutes", path_str, remaining_time);
+        // if we're done visiting stuff, return the score of the path we took
+        if total_time == 0 || visited.len() == self.graph.nodes().len() {
+            // AA DD BB JJ HH EE CC
+            return self.path_score(&visited, total_time);
+        }
 
-        paths
-            .map(|path| self.path_score(&path, start, total_time))
+        // visit every node that hasn't been visited already
+        let valves_to_visit = self.graph.nodes().filter(|valve| !visited.contains(valve));
+
+        // if there are no more valves to visit, spin down the clock and return
+
+        // if there are valves to visit, visit each one there's time to visit
+        let valves_to_visit_at = valves_to_visit.filter_map(|valve| {
+            remaining_time
+                .checked_sub(self.dist(*visited.last().unwrap(), valve) + 1)
+                .map(|arrival_time| (valve, arrival_time))
+        });
+
+        // get the highest score from each of the paths
+        if let Some(max) = valves_to_visit_at
+            .map(|(valve, arrival_time)| {
+                let mut new_visited = visited.clone();
+                new_visited.push(valve);
+                self.visit(new_visited, arrival_time, total_time)
+            })
             .max()
-            .unwrap_or(0)
+        {
+            return max;
+        } else {
+            return self.path_score(&visited, total_time);
+        }
+    }
+
+    fn visit2(
+        &self,
+        tagged: Vec<ValveData<'input>>,
+        activated: Vec<ValveData<'input>>,
+        remaining_time: u32,
+        total_time: u32,
+        players: Vec<Player<'input>>,
+        rate: u32,
+        pressure_released: u32,
+    ) -> (Vec<ValveData>, u32) {
+        let path_str: Vec<&str> = tagged.iter().map(|valve| valve.name).collect();
+
+        let mut remaining_time = remaining_time;
+        let mut pressure_released = pressure_released;
+        let mut rate = rate;
+        let mut players = players.clone();
+        let mut activated = activated.clone();
+
+        // if both players have nowhere else to go
+        if players[0].position.name != START && players[0].dest.is_none() && players[1].position.name != START && players[1].dest.is_none() {
+            pressure_released += rate * remaining_time;
+            return (activated, pressure_released);
+        }
+
+        let one_more = activated.len() + 2 == self.graph.nodes().len();
+
+        // move enough minutes forward until one (or both) player(s) reach a valve.
+
+        let minutes = match (players[0].dest, players[1].dest) {
+            (Some(_), None) if one_more => players[0].ttm,
+            (None, Some(_)) if one_more => players[1].ttm,
+            (Some(_), Some(_)) => players[0].ttm.min(players[1].ttm),
+            _ => 0,
+        };
+
+        // spin time forward for all players to the next valve opening point
+
+        remaining_time -= minutes;
+        if players[0].dest.is_some() {
+            players[0].ttm -= minutes;
+        }
+        if players[1].dest.is_some() {
+            players[1].ttm -= minutes;
+        }
+
+        // open some valves
+
+        pressure_released += rate * minutes;
+
+        // if player 0 arrived at valve _AND_ activated it
+        if let Some(dest) = players[0].dest {
+            if 0 == players[0].ttm {
+                activated.push(dest);
+                rate += dest.rate;
+                players[0].position = players[0].dest.unwrap();
+                players[0].dest = None;
+            }
+        }
+
+        // if player 1 arrived at valve _AND_ activated it
+        if let Some(dest) = players[1].dest {
+            if 0 == players[1].ttm {
+                activated.push(dest);
+                rate += dest.rate;
+                players[1].position = players[1].dest.unwrap();
+                players[1].dest = None;
+            }
+        }
+
+        if activated.len() + 1 == self.graph.nodes().len() || remaining_time == 0 {
+            pressure_released += rate * remaining_time;
+            return (activated, pressure_released);
+        }
+
+        // pick a player's index who is ready to move (one of them will be guaranteed to move)
+
+        let moving_player = if players[0].dest.is_none() { 0 } else { 1 };
+
+        // find the valves that have yet to be opened
+
+        let closed_valves = self.graph.nodes().filter(|valve| !tagged.contains(valve));
+
+        // get the highest score from each of the paths
+
+        if let Some(max) = closed_valves
+            .filter_map(|valve| {
+                // println!("{:?}", path_str);
+                let _p = &path_str; // capture for debugging
+                let mut new_visited = tagged.clone();
+                let mut players = players.clone();
+                let activated = activated.clone();
+                let ttm = self.dist(players[moving_player].position, valve) + 1; // 1 is for activation
+
+                if ttm < remaining_time {
+                    players[moving_player].ttm = ttm;
+                    players[moving_player].dest = Some(valve); // assign their next location; player
+                                                               // will arrive when ttm reaches 0
+                    new_visited.push(valve); // mark the player's next destination as visited so the
+                                             // other player doesn't try to go there too
+
+                    Some(self.visit2(
+                        new_visited,
+                        activated,
+                        remaining_time,
+                        total_time,
+                        players,
+                        rate,
+                        pressure_released,
+                    ))
+                } else {
+                    None
+                }
+            })
+            .max_by(|a, b| {
+                let astr: Vec<&str> = a.0.iter().map(|valve| valve.name).collect();
+                let bstr: Vec<&str> = b.0.iter().map(|valve| valve.name).collect();
+                println!("{:?} -> {}", astr.join(" "), a.1);
+                println!("{:?} -> {}", bstr.join(" "), b.1);
+                a.1.cmp(&b.1)
+            })
+        {
+            return max;
+        } else {
+            // pressure_released += rate * remaining_time;
+            return (activated, pressure_released);
+        }
     }
 
     fn path_score(
         &self,
         path: &Vec<ValveData<'input>>,
-        start: ValveData<'input>,
+        // start: ValveData<'input>,
         total_time: u32,
     ) -> u32 {
-        // initialize these starting values by "visiting" the first valve in the path
-        let path_str: Vec<&str> = path.iter().map(|valve| valve.name).collect();
-        println!("{:?}", path_str);
-        let mut time_left = Some(total_time - self.dist(start, *path.first().unwrap()) - 1);
-        let mut pressure_per_minute = path.first().unwrap().rate;
+        let mut time_left = Some(total_time);
+        let mut pressure_per_minute = 0;
         let mut score = 0;
 
         // now that we _just_ opened the first valve, start visiting the rest of the valves;
@@ -231,7 +390,56 @@ fn part1_solve(input: &str) -> u32 {
     // println!("{:?}", Dot::with_config(&cave.graph, &[]));
 
     if let Some(start) = cave.start {
-        return cave.scan(start, 30);
+        // return cave.scan(start, 30);
+        cave.visit(vec![start], 30, 30)
+    } else {
+        0
+    }
+}
+
+#[aoc(day16, part2)]
+fn part2_solve(input: &str) -> u32 {
+    let mut valves = vec![];
+
+    for line in input.lines() {
+        let mut words = line.split_whitespace();
+        let name = words.nth(1).unwrap();
+        let rate = words
+            .nth(2)
+            .unwrap()
+            .replace("rate=", "")
+            .replace(';', "")
+            .parse::<u32>()
+            .unwrap();
+        words.nth(3).unwrap(); // "tunnels lead to valve"
+        let mut exits = vec![];
+        while let Some(word) = words.next() {
+            exits.push(word.replace(",", ""));
+        }
+        let valve = Valve {
+            data: ValveData { name, rate },
+            exits,
+        };
+        valves.push(valve);
+    }
+
+    let cave = Cave::new(valves);
+
+    // println!("{:?}", Dot::with_config(&cave.graph, &[]));
+
+    if let Some(start) = cave.start {
+        let (path, score) = cave.visit2(
+            vec![start],
+            vec![],
+            26,
+            26,
+            vec![Player::new(start), Player::new(start)],
+            0,
+            0,
+        );
+
+        println!("{:#?}", path);
+        score
     } else {
         0
     }
@@ -258,8 +466,8 @@ Valve JJ has flow rate=21; tunnel leads to valve II";
         assert_eq!(part1_solve(EX), 1651);
     }
 
-    // #[test]
-    // fn part2_test() {
-    //     assert_eq!(part2_solve(EX, 20), 56000011);
-    // }
+    #[test]
+    fn part2_test() {
+        assert_eq!(part2_solve(EX), 1707);
+    }
 }
