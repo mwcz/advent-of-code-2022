dfa7a2c8814ac2a8c5cbd36f5598ba6456f5020d
mwcz <mwc@clayto.com>
2023-01-06 21:01:08 -0500

    some day22 work

--- src/day22.rs
+++ src/day22.rs
@@ -2,33 +2,26 @@ use aoc_runner_derive::aoc;
 use itertools::Itertools;
 use std::{
     collections::{HashMap, VecDeque},
+    ops::Add,
     str::Chars,
 };
 
+type Step = (i32, Dir);
+
 #[derive(Debug)]
-struct Map {
-    grid: Vec<Vec<Cell>>,
-    path: Vec<(i32, Option<Dir>)>,
-}
+struct Steps(Vec<Step>);
 
-impl Map {
-    fn new(input: &str) -> Self {
-        let mut parts = input.split("\n\n");
-
-        let map = parts.next().unwrap();
-
-        let grid: Vec<Vec<Cell>> = map
-            .lines()
-            .map(|line| line.chars().map(Cell::from).collect_vec())
-            .collect();
-
-        let path = parts.next().unwrap().split_inclusive(&['L', 'R']);
-        println!("{:?}", path.clone().collect_vec());
+impl Steps {
+    fn new(path_str: &str) -> Self {
+        let path = path_str.split_inclusive(&['L', 'R']);
         let mut dir = Dir::Right;
-        let path = path
+        let mut steps = vec![];
+        let first = path.next().unwrap();
+        // TODO instead of (dist, Option<dir>), continue converting this to (Dir, dist)
+        let steps = path
             .map(|inst| {
                 let len = inst.len();
-                if ['L', 'R'].contains(&inst.chars().nth(len-1).unwrap()) {
+                if ['L', 'R'].contains(&inst.chars().nth(len - 1).unwrap()) {
                     let num_part = &inst[..inst.len() - 1];
                     let steps = num_part.parse::<i32>().unwrap();
                     dir = dir.turn(&inst[inst.len() - 1..inst.len()]);
@@ -39,19 +32,67 @@ impl Map {
                 }
             })
             .collect_vec();
+        Self(steps)
+    }
+}
 
-        Self {
-            grid,
-            path,
-        }
+#[derive(Debug, Clone, Copy)]
+struct Point(usize, usize);
+
+impl Add<Point> for Point {
+    type Output = Point;
+
+    fn add(self, rhs: Point) -> Self::Output {
+        Self(self.0 + rhs.0, self.1 + rhs.1)
+    }
+}
+
+type Pos = (Point, Dir);
+
+#[derive(Debug)]
+struct Map {
+    grid: Vec<Vec<Cell>>,
+}
+
+impl Map {
+    fn new(map_str: &str) -> Self {
+        let grid: Vec<Vec<Cell>> = map_str
+            .lines()
+            .map(|line| line.chars().map(Cell::from).collect_vec())
+            .collect();
+
+        Self { grid }
     }
 
-    /// Find the starting point on the map. (y, x)
-    fn start_pos(&self) -> (usize, usize) {
+    /// Find the starting point on the map. (x, y)
+    fn start_pos(&self) -> Pos {
         (
-            0,
-            self.grid[0].iter().position(|n| n == &Cell::Open).unwrap(),
+            Point(
+                self.grid[0].iter().position(|n| n == &Cell::Open).unwrap(),
+                0,
+            ),
+            Dir::Right,
         )
+    }
+
+    fn step(&self, cur: &Pos, step: &Step) -> Point {
+        println!("walk from {cur:?} to {step:?}",);
+
+        let dist = step.0;
+        let dir = step.1.unwrap_or(cur.1);
+        let mut pos = cur.0;
+
+        for n in 1..=dist {
+            let maybe = match dir {
+                Dir::Up => Point(pos.0, pos.1),
+                Dir::Right => todo!(),
+                Dir::Down => todo!(),
+                Dir::Left => todo!(),
+            };
+            println!("{n:?}");
+        }
+
+        pos
     }
 }
 
@@ -117,10 +158,20 @@ impl From<char> for Cell {
 }
 
 fn part1_solve(input: &str) -> i32 {
-    let map = Map::new(input);
+    let mut parts = input.split("\n\n");
 
-    println!("{map:#?}",);
-    println!("start: {:?}", map.start_pos());
+    let map = Map::new(parts.next().unwrap());
+    let steps = Steps::new(parts.next().unwrap());
+
+    // println!("{map:#?}",);
+    // println!("{steps:?}",);
+    let mut pos = map.start_pos();
+    println!("start: {pos:?}");
+
+    for step in steps.0 {
+        pos = map.step(&pos, &step);
+    }
+
     0
 }
 
