d0d11d34910c650b3bd7012673ef79f4081576f3
Michael Clayton <mwc@clayto.com>
2023-01-31 16:15:38 -0500

    more day 24 progress, from plane

--- src/day24.rs
+++ src/day24.rs
@@ -1,14 +1,14 @@
 use aoc_runner_derive::aoc;
-use itertools::Itertools;
+use std::fmt::Display;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct Point(usize, usize);
+struct Point(i32, i32);
 
 #[derive(Debug)]
 struct Basin {
-    blizzards: Vec<Cell>,
-    height: u32,
-    width: u32,
+    blizzards: Vec<Blizz>,
+    height: i32,
+    width: i32,
     loc: Point,
     start: Point,
     end: Point,
@@ -19,33 +19,36 @@ impl Basin {
         // parse the input
 
         let mut start: Option<Point> = None;
-        let mut end = Point(0,0);
+        let mut end = Point(0, 0);
+        let mut height = 0;
+        let mut width = 0;
 
-        let blizzards = input
-            .lines()
-            .enumerate()
-            .map(|(y, line)| {
-                line.chars().enumerate().filter_map(|(x, c)| {
-                    let cell = Cell::from((c, x, y));
-                    match cell {
-                        Cell::Wall => None,
-                        Cell::Empty if start.is_none() => {
-                            start = Some(Point(x,y));
-                            None
-                        }
-                        Cell::Empty => {
-                            end = Point(x, y);
-                            None
-                        }
-                        Cell::Up(_) => Some(cell),
-                        Cell::Down(_) => Some(cell),
-                        Cell::West(_) => Some(cell),
-                        Cell::East(_) => Some(cell),
+        let mut blizzards = vec![];
+
+        for (y, line) in input.lines().enumerate() {
+            height = height.max(y as i32);
+            for (x, c) in line.chars().enumerate() {
+                width = width.max(x as i32);
+                let cell = Cell::from((c, x as i32, y as i32));
+                if let Some(blizz_cell) = match cell {
+                    Cell::Empty if start.is_none() => {
+                        start = Some(Point(x as i32, y as i32));
+                        None
                     }
-                })
-            })
-            .flatten()
-            .collect_vec();
+                    Cell::Empty => {
+                        end = Point(x as i32, y as i32);
+                        None
+                    }
+                    Cell::Up(_) => Some(cell),
+                    Cell::Down(_) => Some(cell),
+                    Cell::West(_) => Some(cell),
+                    Cell::East(_) => Some(cell),
+                    _ => None,
+                } {
+                    blizzards.push(Blizz::try_from(&blizz_cell).unwrap());
+                }
+            }
+        }
 
         let loc = start;
 
@@ -62,6 +65,30 @@ impl Basin {
     fn step(&mut self) {}
 }
 
+impl Display for Basin {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for y in 0..=self.height {
+            for x in 0..=self.width {
+                let p = Point(x, y);
+                let blizz = self.blizzards.iter().find(|&b| b.loc == p);
+                if y == 0
+                    || x == 0
+                    || y == self.height
+                    || x == self.width && self.start != p && self.end != p
+                {
+                    write!(f, "#")?;
+                } else if blizz.is_some() {
+                    write!(f, "{}", std::convert::Into::<char>::into(blizz.unwrap()))?;
+                } else {
+                    write!(f, ".")?;
+                }
+            }
+            writeln!(f)?;
+        }
+        Ok(())
+    }
+}
+
 #[derive(Debug, Eq, PartialEq)]
 enum Cell {
     Wall,
@@ -72,23 +99,68 @@ enum Cell {
     East(Point),
 }
 
-impl From<(char, usize, usize)> for Cell {
-    fn from((c, x, y): (char, usize, usize)) -> Self {
+impl From<(char, i32, i32)> for Cell {
+    fn from((c, x, y): (char, i32, i32)) -> Self {
         match c {
             '#' => Cell::Wall,
             '.' => Cell::Empty,
-            '^' => Cell::Up(Point(x,y)),
-            'v' => Cell::Down(Point(x,y)),
-            '<' => Cell::West(Point(x,y)),
-            '>' => Cell::East(Point(x,y)),
+            '^' => Cell::Up(Point(x, y)),
+            'v' => Cell::Down(Point(x, y)),
+            '<' => Cell::West(Point(x, y)),
+            '>' => Cell::East(Point(x, y)),
             _ => panic!("invalid char {c}"),
+        }
+    }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+struct Blizz {
+    loc: Point,
+    dir: Point,
+}
+
+impl Into<char> for &Blizz {
+    fn into(self) -> char {
+        match (self.dir.0, self.dir.1) {
+            (0, 1) => 'v',
+            (0, -1) => '^',
+            (1, 0) => '>',
+            (-1, 0) => '<',
+            _ => 'x',
+        }
+    }
+}
+
+impl TryFrom<&Cell> for Blizz {
+    type Error = ();
+
+    fn try_from(value: &Cell) -> Result<Self, Self::Error> {
+        match value {
+            Cell::Wall => Err(()),
+            Cell::Empty => Err(()),
+            Cell::Up(p) => Ok(Blizz {
+                loc: *p,
+                dir: Point(0, -1),
+            }),
+            Cell::Down(p) => Ok(Blizz {
+                loc: *p,
+                dir: Point(0, 1),
+            }),
+            Cell::West(p) => Ok(Blizz {
+                loc: *p,
+                dir: Point(-1, 0),
+            }),
+            Cell::East(p) => Ok(Blizz {
+                loc: *p,
+                dir: Point(1, 0),
+            }),
         }
     }
 }
 
 fn part1_solve(input: &str) -> i64 {
     let basin = Basin::new(input);
-    println!("{basin:#?}",);
+    println!("{basin}",);
     todo!();
 }
 
